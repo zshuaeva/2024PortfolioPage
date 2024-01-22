4ac37fec691bc1169461d65c9f0ffc0b204d9aa1
Joshua <jpe7619@gmail.com>
2024-01-22 12:10:29 -0800

    update page one to include biography and headshot photo

--- src/PageOne/PageOne.js
+++ src/PageOne/PageOne.js
@@ -1,16 +1,23 @@
 import React from "react";
-import pageOneBackground from "../Components/assets//backgrounds/page-one-background.jpg"
-// import Box from '@mui/material/Box';
-import './PageOne.css'
+import pageOneBackground from "../Components/assets/backgrounds/page-one-background.jpg";
+import './PageOne.css';
+import Biography from '../Components/Biography/Biography.js';
+import headshot from '../Components/assets/projectPhotos/Headshot-2.jpg'
 
 const PageOne = () => {
-  return(
-    <>
-    <div>
-      <img src={pageOneBackground} alt="Description" className="page-one-background" />
+  return (
+    <div className="page-one-container">
+      <div className="biography-app">
+        <Biography />
+      </div>
+      <div className="headshot-img-container">
+          <img src={headshot}  alt='headshot' className="headshot-img" />
+        </div>
+      <div>
+        <img src={pageOneBackground} alt="Description" className="page-one-background" />
+      </div>
     </div>
-    </>
-  )
+  );
 };
 
 export default PageOne;
