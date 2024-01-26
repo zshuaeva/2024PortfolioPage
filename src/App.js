22377728acc99feb04e84c175751fc5b1c773fb3
Joshua <jpe7619@gmail.com>
2024-01-26 13:19:50 -0800

    deploy wepage and add analytics

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import PageOne from './PageOne/PageOne';
@@ -5,7 +6,15 @@ import PageTwo from './PageTwo/PageTwo';
 import PageThree from './PageThree/PageThree';
 import Footer
  from './Components/Footer/Footer';
+ import ReactGA from 'react-ga';
+
 function App() {
+  useEffect(() => {
+
+    ReactGA.initialize('G-66EN66X181');
+
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <div className="App">
       <div className="navigation" id='top-nav'>
