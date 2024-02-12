a33582bf21afd1108221d27aa38cfbacc4919854
Joshua <jpe7619@gmail.com>
2024-02-12 15:14:44 -0800

    update conditional rendering for pages less than 1100px

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import PageOne from './PageOne/PageOne';
 import PageTwo from './PageTwo/PageTwo';
 import PageThree from './PageThree/PageThree';
-import Footer
- from './Components/Footer/Footer';
- import ReactGA from 'react-ga';
+import Footer from './Components/Footer/Footer';
+import ReactGA from 'react-ga';
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
   useEffect(() => {
-
     ReactGA.initialize('G-66EN66X181');
-
     ReactGA.pageview(window.location.pathname + window.location.search);
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
+
   return (
     <div className="App">
       <div className="navigation" id='top-nav'>
         <Navigation />
       </div>
       <div className="page-one">
-        <PageOne />
+            <PageOne />
       </div>
-      <div className="page-two">
-        <PageTwo />
-      </div>
-      <div className="page-three">
-        <PageThree />
-      </div>
-
+      {windowWidth >= 1100 && (
+        <>
+          <div className="page-two">
+            <PageTwo />
+          </div>
+          <div className="page-three">
+            <PageThree />
+          </div>
+        </>
+      )}
       <div className="footer">
         <Footer />
       </div>
-
     </div>
   );
 }
--- src/PageOne/PageOne.js
+++ src/PageOne/PageOne.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import pageOneBackground from "../Components/assets/backgrounds/page-one-background.jpg";
 import './PageOne.css';
 import Biography from '../Components/Biography/Biography.js';
 import headshot from '../Components/assets/projectPhotos/Headshot-2.jpg'
-// import LogoLarge from '../Components/assets/icons/LogoLarge.png'
 
 const PageOne = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="page-one-container">
+      {windowWidth >= 1100 && (
         <Biography />
+      )}
       <div className="headshot-img-container">
-          <img src={headshot}  alt='headshot' className="headshot-img" />
-        </div>
-      <div>
-        <img src={pageOneBackground} alt="Description" className="page-one-background" />
+        <img src={headshot}  alt='headshot' className="headshot-img" />
       </div>
+
+        <div>
+          <img src={pageOneBackground} alt="Description" className="page-one-background" />
+        </div>
+
     </div>
   );
 };
